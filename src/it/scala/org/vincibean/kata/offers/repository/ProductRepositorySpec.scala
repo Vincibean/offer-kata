849d71e248d15c4f566b70e341ce3d5ca2de11a6
Vincibean <andrebessi00@gmail.com>
2018-08-22 00:21:33 +0200

    Added ProductRepository; added corresponding integration test

---
+++ src/it/scala/org/vincibean/kata/offers/repository/ProductRepositorySpec.scala
@@ -0,0 +1,60 @@
+package org.vincibean.kata.offers.repository
+
+import java.util.UUID
+
+import org.specs2.Specification
+import org.specs2.concurrent.ExecutionEnv
+import org.specs2.execute.Result
+import org.specs2.matcher.MatchResult
+import org.specs2.specification.core.SpecStructure
+import org.vincibean.kata.offers.domain.Product
+
+import scala.concurrent.Future
+
+class ProductRepositorySpec(implicit ee: ExecutionEnv) extends Specification {
+  override def is: SpecStructure =
+    sequential ^ s2"""
+         The product repository should
+           return 1 when a new product is saved $s1
+           return a product when its ID is given $s2
+           return all products $s3
+           update a product when a product with the same UUID is saved $s4
+      """
+
+  private val repo = new ProductRepository()
+
+  def s1: MatchResult[Future[Int]] = {
+    repo.insertOrUpdate(mockProduct("s1")) should beEqualTo(1).await
+  }
+
+  def s2: Result = {
+    val mock = mockProduct("s2")
+    val res = for {
+      _ <- repo.insertOrUpdate(mock)
+      x <- repo.findBy(mock.id)
+    } yield x
+    res.map(_ must beSome(mock)).await
+  }
+
+  def s3: Result = {
+    val mock = mockProduct("s3")
+    val res: Future[Seq[Product]] = for {
+      _ <- repo.insertOrUpdate(mock)
+      x <- repo.findAll
+    } yield x
+    res.map(_ must not beEmpty).await
+  }
+
+  def s4: Result = {
+    val mock = mockProduct("s4")
+    val res = for {
+      _ <- repo.insertOrUpdate(mock)
+      _ <- repo.insertOrUpdate(mock.copy(description = "updated product"))
+      x <- repo.findBy(mock.id)
+    } yield x
+    res.map(_ must beSome.which(_.description == "updated product")).await
+  }
+
+  private def mockProduct(descr: String = "a mock product") = Product(UUID.randomUUID(), "mockProduct", descr)
+
+}
---
+++ src/main/scala/org/vincibean/kata/offers/repository/ProductRepository.scala
@@ -0,0 +1,42 @@
+package org.vincibean.kata.offers.repository
+
+import java.util.UUID
+
+import org.vincibean.kata.offers.dao.Tables
+import org.vincibean.kata.offers.domain.{Product => OfferedProduct}
+import slick.basic.DatabaseConfig
+import slick.jdbc.JdbcProfile
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class ProductRepository(implicit ec: ExecutionContext)
+    extends Repository[Future, OfferedProduct, UUID] {
+
+  private val dbConfig: DatabaseConfig[JdbcProfile] =
+    DatabaseConfig.forConfig[JdbcProfile]("storage-config")
+  import dbConfig.profile.api._
+
+  private val db = dbConfig.db
+
+  private val products = Tables.Products
+
+  def insertOrUpdate(p: OfferedProduct): Future[Int] =
+    db.run(products.insertOrUpdate(asRow(p)))
+
+  def findAll: Future[Seq[OfferedProduct]] =
+    db.run(products.result).map(_.map(fromRow))
+
+  def findBy(id: UUID): Future[Option[OfferedProduct]] =
+    db.run(
+      products
+        .filter(_.id === id)
+        .result
+        .headOption
+        .map(_.map(fromRow)))
+
+  private def asRow(prod: OfferedProduct): Tables.ProductsRow =
+    Tables.ProductsRow(prod.id, prod.name, prod.description)
+  private def fromRow(row: Tables.ProductsRow): OfferedProduct =
+    OfferedProduct(row.id, row.name, row.description)
+
+}
